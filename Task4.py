03fc6d50d337e9782f1ec4997b916e207c972d65
Kaminskyi22 <kaminskyi.vlad@icloud.com>
2024-06-20 15:30:53 +0300

    Add files via upload

---
+++ Task4.py
@@ -0,0 +1,19 @@
+day_number=int(input("Enter number of day :"))
+
+match day_number:
+    case 1:
+     print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
+    case _:
+        print("Incorrectly selected day")
---
+++ Task6.py
@@ -0,0 +1,8 @@
+number=int(input ("Please, enter the number: "))
+if number > 0:
+    print("The number is positive")
+elif number < 0:
+    print("The number is negative")
+else:
+    print("The number is zero")
+
